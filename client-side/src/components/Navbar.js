1edb0387da02201f1d24df066b9e3d6304dfa5ee
sandrewtx08 <sandrewtx08@protonmail.com>
2022-01-15 18:49:47 +0000

    navbar (ui)

--- client-side/src/components/Navbar.js
+++ client-side/src/components/Navbar.js
@@ -9,16 +9,13 @@ class Navbar extends Component {
     }
 
     render() {
-        return (<nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
+        return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
                 <Link to='/' className='navbar-brand'>
                     Home
                 </Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
                         <Link to='/login' className='nav-link'>
                             Login
                         </Link>
