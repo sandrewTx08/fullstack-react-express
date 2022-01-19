ffa6f4cab4d414bc17195e4f8e5fa56bb7b7d080
sandrewtx08 <sandrewtx08@protonmail.com>
2022-01-19 14:12:55 +0000

    server-side message

--- server-side/routes/delete.js
+++ server-side/routes/delete.js
@@ -2,12 +2,21 @@ const { Login } = require('../modules/model')
 const body = require('body-parser').json()
 const router = require('express').Router()
 
-router.delete('/', body, async (req, res) => {
-    await Login.findByIdAndDelete(req.body._id)
 
-    res.json({
-        message: `${req.body.username} was deleted.`
-    })
+router.delete('/', body, async (req, res) => {
+    let query = await Login.findByIdAndDelete(req.body._id)
+
+    if (query) {
+        res.json({
+            message: `${query.username} was deleted.`
+        })
+    
+    } else {
+        res.json({
+            error: 'User not found.' 
+        })
+    }
 })
+
 
 module.exports = router
