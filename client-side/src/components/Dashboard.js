3da6eb3ef08f61b30bc2bb92dfd4e547453b2c05
sandrewtx08 <sandrewtx08@protonmail.com>
2022-01-20 23:22:55 +0000

    functional component

--- client-side/src/components/Dashboard.js
+++ client-side/src/components/Dashboard.js
@@ -1,34 +1,34 @@
 import axios from 'axios'
-import { Component } from "react"
+import { useState } from "react"
 
 
-class Dashboard extends Component {
-    constructor() {
-        super()
-        this.state = {}
-        this.getDashboard()
-    }
-
-    getDashboard() {
-        this.setState({ loadingQuery: true })
+export default () => {
+    const getDashboard = () => {
+        setLoadingQuery(true)
         axios({
             url: '/api/dashboard',
             method: 'get'
 
-        }).then(response => {
-            this.setState({ loadingQuery: false })
-            return response
+        }).catch(failed => {
+            return failed.response
 
         }).then(response => {
-            let query = response.data.query
-            this.setState({ query })
-            return response
+            setLoadingQuery(false)
+            return response.data
+
+        }).then(data => {
+            setQuery(data.query)
+
         })
     }
 
-    deleteDocument() {
-        let { _id } = this.state.rowAction
-        this.setState({ loadingQuery: true })
+    const [loadingQuery, setLoadingQuery] = useState('')
+    const [query, setQuery] = useState(() => getDashboard())
+    const [rowAction, setRowAction] = useState('')
+
+    const deleteDocument = () => {
+        let { _id } = rowAction
+        setLoadingQuery(true)
         axios({
             url: '/api/delete',
             method: 'delete',
@@ -39,75 +39,71 @@ class Dashboard extends Component {
         }).catch(failed => {
             return failed.response
 
-        }).then(() => {
-            this.setState({ loadingQuery: false })
+        }).then(response => {
+            setLoadingQuery(false)
+            return response.data
 
-        }).then(() => {
-            this.setState({ rowAction: undefined })
-            this.getDashboard()
+        }).then(data => {
+            getDashboard()
+            setRowAction(null)
         })
     }
 
-    render() {
-        return (<div className='d-flex pt-3 justify-content-center'>
-            {this.state.rowAction
+    return (<div className='d-flex pt-3 justify-content-center' >
+        {rowAction
 
-                // Actions pop-up
-                ? <div className="card w-25">
-                    <div className="card-header">
-                        <h3>Are you want delete this account?</h3>
-                    </div>
-
-                    {/* Ask box */}
-                    <div className="card-body">
-                        <ul class="list-group">
-                            <li class="list-group-item"><b>ID: </b> {this.state.rowAction._id}</li>
-                            <li class="list-group-item"><b>Email: </b>{this.state.rowAction.email}</li>
-                            <li class="list-group-item"><b>Username: </b> {this.state.rowAction.username}</li>
-                        </ul>
-                    </div>
-
-                    <div class="card-footer text-muted d-flex justify-content-center">
-                        <button
-                            type="button"
-                            className="btn btn-danger"
-                            onClick={this.deleteDocument.bind(this)}>Yes, do it now.</button>
-                    </div>
+            // Delete account pop-up
+            ? <div className="card w-25">
+                <div className="card-header">
+                    <h3>Are you want delete this account?</h3>
                 </div>
 
-                // Dashboard table
-                : <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">ID</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.query ? this.state.query.map((document, index) => {
-                            return (<tr>
-                                <th scope="row">{index}</th>
-                                <td>{document._id}</td>
-                                <td>{document.email}</td>
-                                <td>{document.username}</td>
-                                <td>
-                                    <button
-                                        className="btn btn-outline-danger btn-sm"
-                                        type="button"
-                                        onClick={() => { this.setState({ rowAction: document }) }}>
-                                        Delete
-                                    </button>
-                                </td>
-                            </tr>)
-                        }) : undefined}
-                    </tbody>
-                </table>}
-        </div>)
-    }
-}
+                <div className="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item"><b>ID: </b> {rowAction._id}</li>
+                        <li class="list-group-item"><b>Email: </b>{rowAction.email}</li>
+                        <li class="list-group-item"><b>Username: </b> {rowAction.username}</li>
+                    </ul>
+                </div>
 
-export default Dashboard
+                <div class="card-footer text-muted d-flex justify-content-center">
+                    <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={() => deleteDocument()}>Yes, do it now.</button>
+                </div>
+            </div>
+
+            // Dashboard table
+            : query && <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">ID</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {query.map((document, index) => {
+                        return (<tr>
+                            <th scope="row">{index}</th>
+                            <td>{document._id}</td>
+                            <td>{document.email}</td>
+                            <td>{document.username}</td>
+                            <td>
+                                <button
+                                    className="btn btn-outline-danger btn-sm"
+                                    type="button"
+                                    onClick={() => setRowAction(document)}>
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>)
+                    })}
+                </tbody>
+            </table>}
+    </div>)
+}
 
